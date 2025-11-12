61d0b9767929e07bf4ce17afc96f3ee500e07646
Yafigg <Cipto.wongso88@gmail.com>
2025-11-12 23:06:47 +0700

    Second

--- lib/config.dart
+++ lib/config.dart
@@ -1,8 +1,6 @@
 // Centralized app configuration
 
 // Change this to your backend host.
-// Option A (local dev on Android emulator): 'http://10.0.2.2:8000'
-// Option B (hosted backend): 'http://backend-gajayana-kost.throoner.my.id'
-const String apiHostBase = 'http://backend-gajayana-kost.throoner.my.id';
+const String apiHostBase = 'link backendnya taro sini';
 const String apiBaseUrl = '$apiHostBase/api';
 const String storageBaseUrl = '$apiHostBase/storage';
